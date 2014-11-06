09eba23979f44dfa7ff1604a892d48c9c9945929
Andrew.Leo <anderw2007@foxmail.com>
2014-11-06 11:03:20 +0800

    部分修改帮帮公告详情

--- incommunit/Tools/LYAnnouncementDetails.h
+++ incommunit/Tools/LYAnnouncementDetails.h
@@ -14,4 +14,10 @@
     UIImageView *m_image;
 }
 @property(nonatomic,retain)IBOutlet UIImageView *m_image;
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *contentLabel;
+@property (weak, nonatomic) IBOutlet UILabel *timeLabel;
+
+
+
 @end
