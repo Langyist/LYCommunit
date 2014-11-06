dd3c0bcdc92f65c1b77b60cf0e74567889281911
Andrew.Leo <anderw2007@foxmail.com>
2014-11-06 19:24:16 +0800

    增加许可页面

---
+++ incommunit/Tools/LicenseViewController.h
@@ -0,0 +1,13 @@
+//
+//  LicenseViewController.h
+//  incommunit
+//
+//  Created by 李忠良 on 14/11/6.
+//  Copyright (c) 2014年 LANGYI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LicenseViewController : UIViewController
+
+@end
