07b8f8f053f88f983469608bc462b552860ca85c
Andrew.Leo <anderw2007@foxmail.com>
2014-11-08 18:12:48 +0800

    修改网络引擎

--- incommunit/AppDelegate.h
+++ incommunit/AppDelegate.h
@@ -8,7 +8,7 @@
 #import <UIKit/UIKit.h>
 #import <CoreLocation/CLLocationManagerDelegate.h>
 #import "BMapKit.h"
-#define SPECIAL_BLACK [UIColor darkTextColor]//[UIColor colorWithRed:63/255.0f green:63/255.0f blue:62/255.0f alpha:1.0]
+#define SPECIAL_BLACK [UIColor darkTextColor]
 #define TOP_BAR_YELLOW [UIColor colorWithRed:242/255.0f green:178/255.0f blue:59/255.0f alpha:1.0]
 #define SEPLINE_GRAY [UIColor colorWithRed:212/255.0f green:207/255.0f blue:207/255.0f alpha:1.0]
 #define BK_GRAY [UIColor colorWithRed:233/255.0f green:232/255.0f blue:232/255.0f alpha:1.0]
@@ -17,8 +17,13 @@
 #define SPECIAL_GRAY [UIColor grayColor]
 #define SPECIAL_GREEN [UIColor colorWithRed:188/255.0f green:210/255.0f blue:94/255.0f alpha:1.0]
 
+#define ShareAppDelegate (AppDelegate *)[[UIApplication sharedApplication] delegate]
+
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
+
+- (void)waiting:(BOOL)bStart;
+
 @end
 
